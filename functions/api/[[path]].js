4a5764e93c83c4b795433949da5e8a0884c9e5cf
4 0 4 \ 2.0 /🇲🇲 <146018190+nyeinkokoaung404@users.noreply.github.com>
2026-04-23 04:28:50 +0630

    Update [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -35,7 +35,7 @@ const SUSPICIOUS_THRESHOLD = 10;
 
 const ALLOWED_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'];
 const MAX_BODY_SIZE = 50 * 1024 * 1024;
-const ALLOWED_COUNTRIES = ['IR'];
+const ALLOWED_COUNTRIES = [];
 const BLOCKED_COUNTRIES = [];
 const ALLOWED_USER_AGENTS = /telegram|bot|curl|postman|httpie|axios|fetch/i;
 const BLOCKED_USER_AGENTS = /scanner|crawler|spider|bot.*attack|sqlmap|nikto|nmap/i;
